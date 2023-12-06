126b78d98b54a060ce30fa78c4504826ef4241c4
Rohit3003T <152482885+Rohit3003T@users.noreply.github.com>
2023-12-06 22:12:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+        
+        var storedText;
+
+
+        function storeText() {
+            
+            var inputElement = document.getElementById("text");
+
+            
+            storedText = inputElement.value;
+
+
+            var textToEncode = ("Text stored: " + storedText);
+            var qrcode = new QRCode(document.getElementById("qrcode"), {
+                text: textToEncode,
+                width: 250 ,
+                height: 250
+            });
+            
+        }
+        function refreshPage() {
+            location.reload();
+          }
+
+          function downloadPage() {
+            
+            var htmlContent = document.documentElement.outerHTML;
+          
+            
+            var blob = new Blob([htmlContent], { type: 'text/html' });
+          
+            
+            var link = document.createElement('a');
+          
+            
+            link.download = 'page.html';
+          
+
+            link.href = URL.createObjectURL(blob);
+          
+            
+            document.body.appendChild(link);
+          
+            
+            link.click();
+          
+            
+            document.body.removeChild(link);
+          }
+
+           
+
+
